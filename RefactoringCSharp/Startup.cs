40480710af5a4c853ae636402d2781549736f3f4
Dave Mateer <davemateer@gmail.com>
2015-12-10 07:06:20 +0000

    Initial commit

---
+++ RefactoringCSharp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RefactoringCSharp.Startup))]
+namespace RefactoringCSharp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
