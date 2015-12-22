ab2f53cd3ba3516928998c7e421fc7638ab2ed91
Dave Mateer <davemateer@gmail.com>
2015-12-22 16:51:24 +0000

    utc

--- RefactoringCSharp/Controllers/HomeController.cs
+++ RefactoringCSharp/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace RefactoringCSharp.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page. Some change.";
+            ViewBag.Message = "Your application description page. Some change!";
 
             return View();
         }
