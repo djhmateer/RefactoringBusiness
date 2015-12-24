405e7b5af9a66ae5013ab0321cfeadd0168a6d6b
Dave Mateer <davemateer@gmail.com>
2015-12-24 14:56:20 +0000

    typos

--- RefactoringCSharp/Controllers/PageController.cs
+++ RefactoringCSharp/Controllers/PageController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace RefactoringCSharp.Controllers
 {
@@ -10,6 +6,8 @@ namespace RefactoringCSharp.Controllers
     {
         public ActionResult ORM()
         {
+            // Goal here is to read from a database
+
             return View();
         }
     }
