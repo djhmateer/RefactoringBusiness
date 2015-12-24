ebd65439f5e12a720131d9ff48127b52202335c9
Dave Mateer <davemateer@gmail.com>
2015-12-24 15:37:51 +0000

    throw all errors

--- RefactoringCSharp/Global.asax.cs
+++ RefactoringCSharp/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
@@ -12,6 +13,42 @@ namespace RefactoringCSharp
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        //protected void Application_BeginRequest()
+        //{
+        //    //if (Request.IsLocal) {
+        //    MiniProfiler.Start();
+        //    //}
+        //}
+
+        //protected void Application_EndRequest()
+        //{
+        //    MiniProfiler.Stop();
+        //}
+
+        void Application_Error(Object sender, EventArgs e)
+        {
+            //#if DEBUG
+            //Console.WriteLine("Error not handled as in Debug");
+            //#else
+            //var exception = Server.GetLastError();
+
+            //if (exception == null)
+            //{
+            //    return;
+            //}
+            //ILog log = LogManager.GetLogger(typeof(MvcApplication));
+            //log.Error(exception.Message);
+            //log.Error(Request.RawUrl);
+            //log.Error(exception.StackTrace);
+
+            //// Clear the error
+            //Server.ClearError();
+
+            //// Redirect to a landing page
+            //Response.Redirect("/home/error");
+            ////#endif
         }
     }
 }
