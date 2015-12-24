e3ddeb4253e50e23577fcf071c07546a6bb06441
Dave Mateer <davemateer@gmail.com>
2015-12-24 11:02:57 +0000

    caching

--- RefactoringCSharp/Controllers/HomeController.cs
+++ RefactoringCSharp/Controllers/HomeController.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Runtime.Caching;
-using System.Web.Caching;
 using System.Web;
 using System.Web.Mvc;
 
@@ -16,16 +14,40 @@ namespace RefactoringCSharp.Controllers
 
         public ActionResult About()
         {
-            // get buildVersion from Cache if available
-            if (HttpContext.Current.Cache["time"] == null)
+            HttpContext context = System.Web.HttpContext.Current;
+
+            string timeToDisplay;
+            var timeFromCache = context.Cache["time"];
+            if (timeFromCache == null)
             {
-                System.Web.HttpContext.Current.Cache["time"] = DateTime.Now;
+                context.Cache["time"] = DateTime.Now;
+                timeToDisplay = DateTime.Now.ToString();
+            }
+            else
+            {
+                timeToDisplay = timeFromCache + " Cache Hit!";
             }
 
-            ViewBag.Time = ((DateTime)System.Web.HttpContext.Current.Cache["time"]).ToString();
+            ViewBag.Time = timeToDisplay;
 
-            //ViewBag.BuildVersion = fileContent;
 
+            // get buildVersion from Cache if available
+            string buildVersion = context.Cache["BuildVersion"] as string;
+            if (string.IsNullOrEmpty(buildVersion))
+            {
+                using (StreamReader sr = System.IO.File.OpenText(Server.MapPath("~/BuildVersion.txt")))
+                {
+                    buildVersion = sr.ReadToEnd();
+                    context.Cache.Insert("BuildVersion", buildVersion,
+                        new System.Web.Caching.CacheDependency(Server.MapPath("~/SampleFile.txt")));
+                }
+            }
+            else
+            {
+                buildVersion += " Cache Hit!";
+            }
+
+            ViewBag.BuildVersion = buildVersion;
             return View();
         }
 
