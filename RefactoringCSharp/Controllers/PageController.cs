ea333d2015b484640dd36ae5a797b086ec58f469
Dave Mateer <davemateer@gmail.com>
2018-11-22 06:08:15 +0000

    auto commit

--- RefactoringCSharp/Controllers/PageController.cs
+++ RefactoringCSharp/Controllers/PageController.cs
@@ -13,6 +13,7 @@ namespace Dave.RefactoringCSharp.Controllers
         {
             //ViewBag.ConnectionString = ConfigurationManager.ConnectionStrings["DavesJokes"].ToString();
 
+            System.Threading.Thread.Sleep(5000);
             // Read from a database
             //List<Joke> listOfJokes;
             //using (var context = new DavesJokes())
